923365ea50fcafb1620f23693714b2d3e8b1ce94
Francesco Perrino <francescoperrino.mail@gmail.com>
2020-11-09 11:39:32 +0100

    Definition of class Amount

--- budget.py
+++ budget.py
@@ -1,5 +1,28 @@
+class Amount:
+    
+    '''
+    creo una classe Amount che contiene amount e description come da specifiche
+    e salvo negli oggetti di classe Category una lista di oggetti Amount
+    '''
+    tot={"amount":0,"description":""}
+
+    def __init__(self,am,descr=""):
+        tot["amount"]=am
+        tot["description"]=descr
+
+
+
 class Category:
 
+    title=""
+    deposit_list= list()
+
+    
+
+    def __init__(self,name):
+        title=name
+    
+    def deposit(amount,description):
 
 
 
