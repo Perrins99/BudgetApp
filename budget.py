60e38815ae576a18c46e7b7216ead4f4e69ead63
Francesco Perrino <francescoperrino.mail@gmail.com>
2020-11-09 11:43:19 +0100

    Updated Category class

--- budget.py
+++ budget.py
@@ -4,12 +4,14 @@ class Amount:
     creo una classe Amount che contiene amount e description come da specifiche
     e salvo negli oggetti di classe Category una lista di oggetti Amount
     '''
-    tot={"amount":0,"description":""}
+    tot={}
 
     def __init__(self,am,descr=""):
-        tot["amount"]=am
-        tot["description"]=descr
-
+        self.tot["amount"]=am
+        self.tot["description"]=descr
+    
+    def getTot(self):
+        return self.tot
 
 
 class Category:
@@ -22,9 +24,9 @@ class Category:
     def __init__(self,name):
         title=name
     
-    def deposit(amount,description):
-
-
+    def deposit(self,amount,description):
+        dep=Amount(amount,description)
+        self.deposit_list.append(dep)
 
 
 def create_spend_chart(categories):
