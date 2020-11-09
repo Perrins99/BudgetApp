b6e3b455704d9b38749419eccb927920a12c98fe
Francesco Perrino <francescoperrino.mail@gmail.com>
2020-11-09 11:59:36 +0100

    Done first 3 points of the readme file

--- budget.py
+++ budget.py
@@ -18,6 +18,7 @@ class Category:
 
     title=""
     deposit_list= list()
+    balance=0
 
     
 
@@ -27,7 +28,19 @@ class Category:
     def deposit(self,amount,description):
         dep=Amount(amount,description)
         self.deposit_list.append(dep)
+        self.balance+=dep.getTot()["amount"]
+    
+    def withdraw(self,amount,description):
+        wd=Amount(-1*amount,description)
+        if self.balance-amount>=0:
+            self.deposit_list.append(wd)
+            self.balance+=wd.getTot()["amount"]
+            return True
+        else:
+            return False
 
+    def get_balance(self):
+        return self.balance
 
 def create_spend_chart(categories):
     return
